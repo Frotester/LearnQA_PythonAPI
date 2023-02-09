176262d02ca064577e8fc33d3afbb54711f07b00
Надежда Шахова <la.nadezhda2016@yandex.ru>
2023-02-09 20:42:05 +0300

    Lesson1_Ex4.

--- Lesson1_Ex4.py
+++ Lesson1_Ex4.py
@@ -4,5 +4,9 @@ import requests
 # response = requests.get("http://playground.learnqa.ru/api/hello", params=payload)
 # print(response.text)
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+# response = requests.get("https://playground.learnqa.ru/api/get_text")
+# print(response.text)
+
+
+response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
