d4cd363709551419b562cb43cdb9fdb886a8ee75
Надежда Шахова <la.nadezhda2016@yandex.ru>
2023-02-09 20:43:01 +0300

    Lesson1_Ex4.

--- Lesson1_Ex4.py
+++ Lesson1_Ex4.py
@@ -1,12 +1,13 @@
+# Ex4: GET-запрос
+# В проекте создать скрипт, который отправляет GET-запрос по адресу: https://playground.learnqa.ru/api/get_text
+#
+# Затем с помощью функции print вывести содержимое текста в ответе на запрос. Когда скрипт будет готов - давайте его закоммитим в наш репозиторий.
+#
+# Результатом должна быть ссылка на коммит.
+
+
+
 import requests
-
-# payload = {"name": "User1"}
-# response = requests.get("http://playground.learnqa.ru/api/hello", params=payload)
-# print(response.text)
-
-# response = requests.get("https://playground.learnqa.ru/api/get_text")
-# print(response.text)
-
 
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
