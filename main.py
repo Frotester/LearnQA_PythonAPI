cd6428c49e02dd123817ff6576acb7df7678254c
Надежда Шахова <la.nadezhda2016@yandex.ru>
2023-02-09 20:32:25 +0300

    Lesson1_Ex4.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
-payload = {"name": "User1"}
-response = requests.get("http://playground.learnqa.ru/api/hello", params=payload)
+# payload = {"name": "User1"}
+# response = requests.get("http://playground.learnqa.ru/api/hello", params=payload)
+# print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
